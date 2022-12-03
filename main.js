510505d34a69fc8b19272e8eaf649306c3e4b412
Danish <dan71ish@gmail.com>
2022-12-03 20:10:34 +0530

    Navigation added

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const buttons = document.querySelectorAll('button');
+let list = document.querySelector('.items .list')
 buttons.forEach((button) => {
 button.addEventListener('click',() => { 
     const CurrentState = button.getAttribute('data-state');   
     if(!CurrentState || CurrentState === 'closed'){
         button.setAttribute('aria-expanded', 'true');
         button.setAttribute('data-state','opened')
+        list.classList.add('open');
     }
     else{
         button.setAttribute('aria-expanded', 'false');
         button.setAttribute('data-state','closed');
+        list.classList.remove('open');
     }
 })
 })
